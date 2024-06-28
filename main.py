6b142e9359ab63518f3f983360b3383e82456f6e
Bjarne Stargardt <b.opensource@gmail.com>
2024-06-28 22:16:30 +0200

    bugfixes and improvements

--- main.py
+++ main.py
@@ -13,19 +13,17 @@ from config import *
 class SongAutoClicker:
     def __init__(self):
         self.songlist = {}
-        self.song_order = tuple(self.songlist.keys())
+        self.song_order = ()
         self.compatible_songs = {}
 
         self.current_song_index = -1
-        try:
-            self.current_song = self.song_order[self.current_song_index]
-            self.current_song_data = self.songlist[self.current_song]
-        except IndexError:
-            self.current_song = None
-            self.current_song_data = None
         self.current_verse_index = -1
 
         self.status_overlay = None
+
+    @property
+    def current_song_data(self):
+        return self.songlist[self.song_order[self.current_song_index]]
 
     # def setup_voice_recognition(self, vosk_model_path=vosk_model_path):
     #     audio = pyaudio.PyAudio()
@@ -70,7 +68,6 @@ class SongAutoClicker:
         self.overlay_thread.start()
         print("Overlay started")
         self.status_overlay = self.overlay_thread.get_status_overlay()
-        print(type(self.status_overlay))
 
     @staticmethod
     def loop_indexes(index, length):
@@ -88,12 +85,10 @@ class SongAutoClicker:
         return self.loop_indexes(self.current_song_index - 1, len(self.song_order))
 
     def next_verse_index(self):
-        return self.loop_indexes(self.current_verse_index + 1, len(self.songlist[self.current_song].get('verse_order')))
+        return self.loop_indexes(self.current_verse_index + 1, len(self.current_song_data.get('verse_order')))
 
     def previous_verse_index(self):
-        return self.loop_indexes(self.current_verse_index - 1, len(self.songlist[self.current_song].get('verse_order')))
-
-
+        return self.loop_indexes(self.current_verse_index - 1, len(self.current_song_data.get('verse_order')))
 
     # Hotkey Callbacks
     def call_next_song(self):
@@ -147,6 +142,7 @@ class SongAutoClicker:
                     self.songlist.update({songname: song_data})
                 else:
                     raise Exception("Song " + songname + " is not compatible.")
+        self.song_order = tuple(self.songlist.keys())
 
     def load_song(self, songname):
         sng_filename = self.compatible_songs[songname]
@@ -165,14 +161,15 @@ class SongAutoClicker:
                         song_data.update({"titel": line.replace("#Title=", "").strip()})
                     elif line.startswith("#VerseOrder="):
                         song_data.update({"verse_order": line.replace("#VerseOrder=", "").split(",")})
-                elif line.strip() == ("--" or "---"):
+                    else:
+                        continue
+                elif line == "--" or line == "---":
                     lastline = "splitter"
                     if len(verse) > 0:
                         song_data.update({versename: verse})
                         verse = []
                         versename = ""
                 else:
-                    line = line.strip()
                     if lastline == "splitter":
                         versename = line
                         lastline = "versename"
@@ -181,6 +178,9 @@ class SongAutoClicker:
                         lastline = "verse"
                     elif lastline == "verse":
                         verse.append(line)
+
+                if len(verse) > 0:
+                    self.songlist.update({songname: song_data})
 
         return song_data
 
